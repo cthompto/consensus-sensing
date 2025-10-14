c565258460e19dbabd6b5e80559df8d91b9eb58c
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2025-10-14 12:45:15 -0400

    initial upload

---
+++ sketch.js
@@ -0,0 +1,127 @@
+let frameImage;
+let canvas;
+let baseImage = [];
+let overlayOne = [];
+let overlayTwo = [];
+let overlayThree = [];
+let imageIndex = 0;
+let layerIndex = 0;
+let state = "standard";
+
+// states to use = "title", "standard", "info", "data"];
+
+// filter testing
+let sampleFilter;
+
+function preload() {
+  frameImage = loadImage("omni-data-test.png");
+  for (let i = 0; i < 4; i++) {
+    baseImage[i] = loadImage("test-assets/t" + i + "0.png");
+    overlayOne[i] = loadImage("test-assets/t" + i + "1.png");
+    overlayTwo[i] = loadImage("test-assets/t" + i + "2.png");
+    overlayThree[i] = loadImage("test-assets/t" + i + "3.png");
+  }
+}
+
+function setup() {
+  canvas = createCanvas(640, 360, WEBGL);
+  rectMode(CENTER);
+  sampleFilter = createFilterShader(fip.halftone);
+}
+
+function draw() {
+  background(0);
+
+  imageRender();
+  image(frameImage, -width/2, -height/2, width, height);
+  
+  //Filter application
+  //filter(sampleFilter);
+  //sampleFilter.setUniform('tex0', )
+  //sampleFilter.setUniform('cellSize', 6);
+  //sampleFilter.setUniform('threshold', 0.1);
+
+}
+
+// for images displayed on "screen"
+function imageRender() {
+  image(
+    baseImage[imageIndex],
+    -width * 0.45,
+    -height * 0.4,
+    width * 0.45,
+    width * 0.45
+  );
+
+  if (layerIndex == 1) {
+    image(
+      overlayOne[imageIndex],
+      -width * 0.45,
+      -height * 0.4,
+      width * 0.45,
+      width * 0.45
+    );
+  } else if (layerIndex == 2) {
+    image(
+      overlayTwo[imageIndex],
+      -width * 0.45,
+      -height * 0.4,
+      width * 0.45,
+      width * 0.45
+    );
+  } else if (layerIndex == 3) {
+    image(
+      overlayThree[imageIndex],
+      -width * 0.45,
+      -height * 0.4,
+      width * 0.45,
+      width * 0.45
+    );
+  }
+}
+
+function mousePressed() {
+  //button row 1 - power / reset / print
+
+  //button row 2 - layer up
+  if (mouseY >= height * 0.22 && mouseY <= height * 0.33) {
+    if (mouseX >= width * 0.75 && mouseX <= width * 0.8125) {
+      layerIndex++;
+      if (layerIndex >= baseImage.length - 1) {
+        layerIndex = baseImage.length - 1;
+      }
+      console.log(layerIndex);
+    }
+  }
+
+  //button row 3 - back and forward
+  if (mouseY >= height * 0.38 && mouseY <= height * 0.5) {
+    if (mouseX >= width * 0.84375 && mouseX <= width * 0.90625) {
+      imageIndex++;
+      if (imageIndex >= baseImage.length - 1) {
+        imageIndex = baseImage.length - 1;
+      }
+      layerIndex = 0;
+    } else if (mouseX >= width * 0.65625 && mouseX <= width * 0.71875) {
+      imageIndex--;
+      console.log("test");
+      if (imageIndex <= 0) {
+        imageIndex = 0;
+      }
+      layerIndex = 0;
+    }
+  }
+
+  //button row 4 - layer down
+  if (mouseY >= height * 0.55 && mouseY <= height * 0.66) {
+    if (mouseX >= width * 0.75 && mouseX <= width * 0.8125) {
+      layerIndex--;
+      if (layerIndex <= 0) {
+        layerIndex = 0;
+      }
+      console.log(layerIndex);
+    }
+  }
+
+  //button row 5 - info / data
+}
