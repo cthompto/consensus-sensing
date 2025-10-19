838988d9ab0cbb036857bf3560bc30f5cc795197
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2025-10-19 18:15:18 -0400

    update interface

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ let overlayThree = [];
 let imageIndex = 0;
 let layerIndex = 0;
 let state = "standard";
+let glitchAmount = 0;
 
 // states to use = "title", "standard", "info", "data"];
 
@@ -14,7 +15,7 @@ let state = "standard";
 let sampleFilter;
 
 function preload() {
-  frameImage = loadImage("omni-data-test.png");
+  frameImage = loadImage("omni-data-test-2.png");
   for (let i = 0; i < 15; i++) {
     baseImage[i] = loadImage("assets/" + i + "a.png");
     overlayOne[i] = loadImage("assets/" + i + "b.png");
@@ -26,7 +27,11 @@ function preload() {
 function setup() {
   canvas = createCanvas(windowWidth, windowWidth*0.5625, WEBGL);
   rectMode(CENTER);
-  sampleFilter = createFilterShader(fip.crt);
+  sampleFilter = createFilterShader(fip.glitch);
+
+  glitchAmount = int(random(-30,30));
+  glitchAmount = glitchAmount/10;
+  console.log(glitchAmount);
 }
 
 function draw() {
@@ -34,17 +39,22 @@ function draw() {
 
   imageRender();
   //Filter image portion
-  //filter(sampleFilter);
+  filter(sampleFilter);
   
   //glitch settings
-  //sampleFilter.setUniform('glitchIntensity', 0.0);
+  sampleFilter.setUniform('glitchIntensity', glitchAmount);
   
   //crt settings
   //sampleFilter.setUniform('time', millis() * 0.0001);
 
-
-
+  //device frame
   image(frameImage, -width/2, -height/2, width, height);
+
+  //title overlay
+  if(state == "title") {
+
+  }
+
 }
 
 // for images displayed on "screen"
@@ -87,10 +97,32 @@ function imageRender() {
 }
 
 function mousePressed() {
-  //button row 1 - power / reset / print
+  //button row 1 - focus - / focus + / print
+  if (mouseY >= height * 0.055 && mouseY <= height * 0.11) {
+
+    //focus - 
+    if (mouseX >= width * 0.625 && mouseX <= width * 0.6875) {
+      glitchAmount = glitchAmount-0.1;
+      console.log(glitchAmount);
+    }
+
+    //focus +
+    if (mouseX >= width * 0.75 && mouseX <= width * 0.8125) {
+      glitchAmount = glitchAmount+0.1;
+      console.log(glitchAmount);
+    }
+
+    //print
+    if (mouseX >= width * 0.875 && mouseX <= width * 0.9375) {
+      printImg = canvas.get(width*0.0625,height*0.11,width*0.4375,width*0.4375);
+      printImg.save('consensus-print', 'png');
+   }
+  }
+
 
   //button row 2 - layer up
   if (mouseY >= height * 0.22 && mouseY <= height * 0.33) {
+    //layer up
     if (mouseX >= width * 0.75 && mouseX <= width * 0.8125) {
       layerIndex++;
       if (layerIndex >= baseImage.length - 1) {
@@ -102,13 +134,17 @@ function mousePressed() {
 
   //button row 3 - back and forward
   if (mouseY >= height * 0.38 && mouseY <= height * 0.5) {
+    //forward
     if (mouseX >= width * 0.84375 && mouseX <= width * 0.90625) {
       imageIndex++;
       if (imageIndex >= baseImage.length) {
         imageIndex = 0;
       }
       layerIndex = 0;
-    } else if (mouseX >= width * 0.65625 && mouseX <= width * 0.71875) {
+    }
+    
+    //back
+    if (mouseX >= width * 0.65625 && mouseX <= width * 0.71875) {
       imageIndex--;
       console.log("test");
       if (imageIndex < 0) {
@@ -120,6 +156,7 @@ function mousePressed() {
 
   //button row 4 - layer down
   if (mouseY >= height * 0.55 && mouseY <= height * 0.66) {
+    //layer down
     if (mouseX >= width * 0.75 && mouseX <= width * 0.8125) {
       layerIndex--;
       if (layerIndex <= 0) {
@@ -130,6 +167,30 @@ function mousePressed() {
   }
 
   //button row 5 - info / data
+
+  if (mouseY >= height * 0.722 && mouseY <= height * 0.77) {
+
+    //data 
+    if (mouseX >= width * 0.625 && mouseX <= width * 0.6875) {
+      if(state == "data") {
+        state ="standard"
+      } else {
+        state = "data";
+      }
+      console.log(state);
+    }
+
+    //info
+    if (mouseX >= width * 0.875 && mouseX <= width * 0.9375) {
+      if(state == "info") {
+        state ="standard"
+      } else {
+        state = "info";
+      }
+      console.log(state);
+   }
+  }
+
 }
 
 
